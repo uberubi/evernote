017b0e62c0bb5201044f13256979005e321ab7d9
Andrew A. Stenipanin <and5251@yandex.ru>
2020-07-21 01:06:54 +0300

    firebase deploy

---
+++ dist/precache-manifest.9717c5d19035771936b19a50a8f7b537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8901b99c24e7cfbd9201e72d06c198",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb178ef47ea912581f21",
+    "url": "/static/css/main.60d5545d.chunk.css"
+  },
+  {
+    "revision": "9ed6a73294280ee79830",
+    "url": "/static/js/2.30eb5ec0.chunk.js"
+  },
+  {
+    "revision": "e66d8bfcf01b9d69124b67e3e620fab0",
+    "url": "/static/js/2.30eb5ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb178ef47ea912581f21",
+    "url": "/static/js/main.b7634779.chunk.js"
+  },
+  {
+    "revision": "a2b9b4a5a158b9709148",
+    "url": "/static/js/runtime-main.fa133e06.js"
+  }
+]);
